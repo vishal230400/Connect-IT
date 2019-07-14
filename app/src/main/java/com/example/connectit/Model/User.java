4e30090a991053c2d1c9422119ab8d63bf910fc1
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2019-07-14 12:43:55 +0530

    post

---
+++ app/src/main/java/com/example/connectit/Model/User.java
@@ -0,0 +1,41 @@
+package com.example.connectit.Model;
+
+public class User {
+    public String id;
+    public String username;
+    public String fullname;
+    public String bio;
+    public String imageurl;
+
+    public User()
+    {}
+
+    public String getId() {
+        return id;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public String getImageurl() {
+        return imageurl;
+    }
+
+    public User(String id, String username, String fullname, String bio, String imageurl) {
+        this.id = id;
+        this.username = username;
+        this.fullname = fullname;
+        this.bio = bio;
+        this.imageurl = imageurl;
+    }
+}
